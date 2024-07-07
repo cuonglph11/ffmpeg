d876f3a91d71e0fe1856b3b398abb6033fffe135
ngovankhai <ngovankhai2000@gmail.com>
2024-07-07 21:23:16 +0700

    add optional option save results

--- index.js
+++ index.js
@@ -36,40 +36,33 @@ const processVideoBuffer = (buffer, outputUniqueDir, isSaved) => {
       ])
       .on("end", async () => {
         try {
-          const uniqueImages = await filterUniqueFrames(
+          const uniqueBufferImages = await filterUniqueFrames(
             originalFramesDir,
             outputUniqueDir
           );
 
-          // Read unique images into buffers
-          //   const buffers = await Promise.all(
-          //     uniqueImages.map(async (imagePath) => {
-          //       return fs.promises.readFile(imagePath);
-          //     })
-          //   );
+          //   Delete original frames
+          fs.readdir(originalFramesDir, (err, files) => {
+            if (err) throw err;
+            for (const file of files) {
+              fs.unlink(path.join(originalFramesDir, file), (err) => {
+                if (err) throw err;
+              });
+            }
+          });
 
-          // Delete original frames
-          //   fs.readdir(originalFramesDir, (err, files) => {
-          //     if (err) throw err;
-          //     for (const file of files) {
-          //       fs.unlink(path.join(originalFramesDir, file), (err) => {
-          //         if (err) throw err;
-          //       });
-          //     }
-          //   });
+          if (isSaved) {
+            // Save unique images to disk
+            uniqueBufferImages.forEach((imgBuffer, index) => {
+              const outputPath = path.join(
+                outputUniqueDir,
+                `unique-frame-${index}.png`
+              );
+              fs.promises.writeFile(outputPath, imgBuffer);
+            });
+          }
 
-          //   if (isSaved) {
-          //     // Save unique images to disk
-          //     uniqueImages.forEach((imagePath, index) => {
-          //       const outputPath = path.join(
-          //         outputUniqueDir,
-          //         `unique-frame-${index}.png`
-          //       );
-          //       fs.promises.writeFile(outputPath, buffers[index]);
-          //     });
-          //   }
-
-          resolve(uniqueImages);
+          resolve(uniqueBufferImages);
         } catch (error) {
           reject(error);
         }
@@ -81,10 +74,11 @@ const processVideoBuffer = (buffer, outputUniqueDir, isSaved) => {
   });
 };
 
-const filterUniqueFrames = async (framesDir, outputUniqueDir) => {
+const filterUniqueFrames = async (originalFramesDir, outputUniqueDir) => {
   const frameFiles = fs
-    .readdirSync(framesDir)
+    .readdirSync(originalFramesDir)
     .filter((file) => file.endsWith(".png"));
+
   let prevImage = null;
   let uniqueFrameCount = 0;
   const uniqueImages = [];
@@ -92,42 +86,24 @@ const filterUniqueFrames = async (framesDir, outputUniqueDir) => {
   const framesLength = frameFiles.length;
 
   for (let i = 0; i < framesLength; i++) {
-    const framePath = path.join(framesDir, frameFiles[i]);
+    const framePath = path.join(originalFramesDir, frameFiles[i]);
     try {
-      console.time("load image");
       const img = await Jimp.read(framePath);
-      console.timeEnd("load image");
 
       if (prevImage) {
-        console.time("diff");
         const diff = Jimp.diff(prevImage, img).percent;
-        console.timeEnd("diff");
 
         if (diff > diffThreshold) {
-          // Adjust threshold based on your needs
-          console.time("write image");
           const buffer = await img.getBufferAsync(Jimp.MIME_PNG);
-          console.timeEnd("write image");
 
           uniqueImages.push(buffer);
-          const uniqueFramePath = path.join(
-            outputUniqueDir,
-            `unique-frame-${uniqueFrameCount}.png`
-          );
 
-          console.time("write image");
-          await img.writeAsync(uniqueFramePath);
-          console.timeEnd("write image");
           uniqueFrameCount++;
         }
       } else {
         const buffer = await img.getBufferAsync(Jimp.MIME_PNG);
         uniqueImages.push(buffer);
-        const uniqueFramePath = path.join(
-          outputUniqueDir,
-          `unique-frame-${uniqueFrameCount}.png`
-        );
-        await img.writeAsync(uniqueFramePath);
+
         uniqueFrameCount++;
       }
       prevImage = img;
@@ -142,13 +118,11 @@ const filterUniqueFrames = async (framesDir, outputUniqueDir) => {
 const exampleUsage = async () => {
   const videoBuffer = fs.readFileSync("dog.mp4");
   try {
-    console.time("start run");
     const uniqueImages = await processVideoBuffer(
       videoBuffer,
       outputUniqueDir,
       true
     );
-    console.timeEnd("start run");
     console.log("Unique images processed:", uniqueImages.length);
     // uniqueImages contains buffers of unique frames
   } catch (error) {
